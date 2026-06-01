92fa2b4e92db4d3d34744c6480149f7a6829b7b4
Aisha-shaikh578 <itsaishashaikh05@gmail.com>
2026-06-01 23:21:25 +0530

    Completed styling StatsCard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,11 @@
 import Header from '@/components/Header'
 import Sidebar from '@/components/Sidebar'
-import React from 'react'
+import StatsCard from "@/components/StatsCard";
+import {
+  FiFileText,
+  FiUpload,
+  FiBookmark,
+} from "react-icons/fi";
 
 export default function page() {
   return (
@@ -10,7 +15,42 @@ export default function page() {
         <Header />
 
         <main className='flex-1 p-6'>
-          Dashboard Content
+            <div className='flex flex-col md:flex-row justify-between items-center mb-5'>
+              <h1 className="mb-4 text-lg md:text-2xl font-semibold md:mt-3">
+                Welcome back, Aisha
+              </h1>
+
+              <button className='bg-[#4338ca] text-white px-4 py-2 rounded flex gap-2 items-center cursor-pointer hover:opacity-50'>
+                Upload
+                <span>{<FiUpload />}</span>
+              </button>
+            </div>
+
+            <div className="grid gap-5 sm:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-6">
+              <StatsCard
+                title="Total Resources"
+                value={128}
+                icon={<FiFileText />}
+                iconColor="text-yellow-700"
+                bgColor="bg-yellow-50"
+              />
+
+              <StatsCard
+                title="My Uploads"
+                value={43}
+                icon={<FiUpload />}
+                iconColor="text-sky-700"
+                bgColor="bg-sky-50"
+              />
+
+              <StatsCard
+                title="Bookmarks"
+                value={12}
+                icon={<FiBookmark />}
+                iconColor="text-green-700"
+                bgColor="bg-green-50"
+              />
+            </div>
         </main>
       </div>
     </div>
---
+++ components/StatsCard.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+type StatsCardProps = {
+  title: string;
+  value: number;
+  icon: React.ReactNode;
+  iconColor: string;
+  bgColor: string;
+};
+
+export default function StatsCard({
+  title,
+  value,
+  icon,
+  iconColor,
+  bgColor,
+}: StatsCardProps) {
+  return (
+    <div
+      className={`rounded-xl p-5 shadow-sm border max-w-50 ${bgColor}`}
+    >
+      <div className={`mb-4 text-2xl ${iconColor}`}>
+        {icon}
+      </div>
+
+      <h3 className="text-gray-700 text-lg">
+        {title}
+      </h3>
+
+      <p className="mt-2 text-4xl font-bold">
+        {value}
+      </p>
+    </div>
+  );
+}
